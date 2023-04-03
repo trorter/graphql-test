0a1a6e5362d7f7756ea2861cd7ac3c630fdf6fe6
AndreiL <andrei.ledovskikh@network.global>
2023-04-03 07:13:00 +0400

    Initial commit

---
+++ src/main/java/ay/ledov/graphqltest/bookDetails/BookController.java
@@ -0,0 +1,20 @@
+package ay.ledov.graphqltest.bookDetails;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.getAuthorId());
+    }
+}
---
+++ src/test/java/ay/ledov/graphqltest/GraphqlTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ay.ledov.graphqltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
